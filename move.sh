0d428b681d8f8143f6823d965028bd80a4ef0382
lchsk <mjlechowski@gmail.com>
2014-12-24 16:57:23 +0000

    Script takes a paramter which specifies source directory

--- move.sh
+++ move.sh
@@ -4,26 +4,33 @@ PROGRAM="move"
 PROGRAM_EXT="sh"
 OPERATION="mv"
 
-dir=${PWD}
+if [ $1 != "" ]; then
+  dir=$1
+else
+  dir=${PWD}
+fi;
+
 dirs_created=0
 files_moved=0
+
+echo "Moving files in" $dir "directory."
 
 for file in $dir/*
 do
   filename=$(basename "$file")
   extension="${file##*.}"
-  filename="${filename%.*}"
+  name="${filename%.*}"
   
   if [ ! -d "$file" ]; then
     # only files, please
       
     if [ "$extension" != "$PROGRAM_EXT" ]; then
-      mkdir -p $extension;
+      mkdir -p "$dir""$extension";
       dirs_created=$((dirs_created+1));
     fi;
 
-    if [ "$filename" != "$PROGRAM" ]; then
-      $OPERATION "$file" "$extension"/"$filename";
+    if [ "$name" != "$PROGRAM" ]; then
+      $OPERATION "$file" "$dir"/"$extension"/"$filename";
       files_moved=$((files_moved+1));
     fi;
 
